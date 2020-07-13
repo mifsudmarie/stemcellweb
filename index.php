c32671e841fda0dcf652fa837d1b918cedac8790
mifsudmarie <68011481+mifsudmarie@users.noreply.github.com>
2020-07-13 21:34:38 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+	<section class="inventors-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="background-container">
+						<div class="row">
+							<div class="col-10 offset-1">
+								<h2>Inventors</h2>
+							</div>
+							<div class="inventor row">
+								<div class="col-1 offset-1">
+
+								</div>
+								<div class="col-3">
+								</div>
+								<div class="col-5 offset-1">
+									<h3>Prof. Pierre Schembri Wismayer</h3>
+									<p>Prof Schembri-Wismayer has been involved in bringing in more than 1 million worth of grants funding to the University.. His research develops novel methods of immunotherapy and differentiation as cancer therapeutics. He is also developing new methods of producing pluripotent stem cells. He has a strong interest in anatomical biomechanics, where he is developing a novel hip joint, which is also patented. He supervises and has supervised several Ph.D’s and Masters students. He is regular post-graduate thesis examiner. He teaches undergraduate students in the Faculty of Medicine and Surgery and the Faculty of Health sciences in Gross Anatomy and Cell Biology. He also contributes to a number of post-graduate medical and scientific courses.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-12 col-md-3">
+					<img src="assets/img/logo.png" height="55px"/>
+				</div>
+				<div class="col-12 offset-0 col-md-5 offset-md-4">
+					<h3>Contact</h3>
+					<p>
+						Licensing and Industrial Partnerships:<br>
+						Marie Mifsud<br>
+						Executive, Licensing<br>
+						Knowledge Transfer Office, University of Malta<br>
+						marie.mifsud@um.edu.mt<br>
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
