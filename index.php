79cfa0e20632b995f04dcc05cec544e5ca6e5a2d
mifsudmarie <68011481+mifsudmarie@users.noreply.github.com>
2020-07-15 21:37:33 +0200

    New Changes

--- index.php
+++ index.php
@@ -3,63 +3,173 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+		<section class="navbar-default bg-white">
+	<div class="container">
+	<nav class="navbar navbar-expand-lg navbar-light">
+	    <div class="d-flex flex-grow-1">
+	        <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
+	        <a class="navbar-brand d-none d-lg-inline-block" href="#">
+	            <img class=pr-4 mr-1 src="assets/img/DeNovo_Blue.svg" height="55px"/>
+	        </a>
+	        <a class="navbar-brand-two mx-auto d-lg-none d-inline-block" href="#">
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-	<section class="inventors-section">
-		<div class="container">
+	        </a>
+	        <div class="pr-5 mr-5 w-100 text-right">
+	            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
+	                <span class="navbar-toggler-icon"></span>
+	            </button>
+	        </div>
+	    </div>
+	    <div class="pl-5 ml-5 collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+				<span style="font-family: gillsans">
+	        <ul class="navbar-nav ml-auto flex-nowrap pl-5 ml-5">
+	            <li class="nav-item">
+	                <a href="#" class="nav-link m-2 menu-item nav-active">About</a>
+	            </li>
+	            <li class="nav-item">
+	                <a href="#" class="nav-link m-2 menu-item">Technology</a>
+	            </li>
+	            <li class="nav-item">
+	                <a href="#" class="nav-link m-2 menu-item">Novelty</a>
+	            </li>
+							<li class="nav-item">
+	                <a href="#" class="nav-link m-2 menu-item">Inventors</a>
+	            </li>
+	            <li class="nav-item">
+	                <a href="#" class="nav-link m-2 menu-item">Contact</a>
+	            </li>
+	        </ul>
+				</span>
+	    </div>
+	</nav>
+		</section>
+	<section class="home">
+		<div class="container pt-3 mt-4">
 			<div class="row">
 				<div class="col-12">
-					<div class="background-container">
-						<div class="row">
-							<div class="col-10 offset-1">
-								<h2>Inventors</h2>
+					<div class="background-white">
+						<div class="row d-flex align-items-center">
+							<div class="col-5 offset-1">
+								<span style="font-family: gillsans"><h1 style="color:#0B0062;font-size:74px" class="semi";><i>de novo</i> Cell</h1>
+									<h3 style="color:#0B0062;font-size:30px" class="pb-5 mb-5">--------------Chemical production of induced pluripotent stem cells</h3></span>
 							</div>
-							<div class="inventor row">
-								<div class="col-1 offset-1">
-
-								</div>
-								<div class="col-3">
-								</div>
-								<div class="col-5 offset-1">
-									<h3>Prof. Pierre Schembri Wismayer</h3>
-									<p>Prof Schembri-Wismayer has been involved in bringing in more than 1 million worth of grants funding to the University.. His research develops novel methods of immunotherapy and differentiation as cancer therapeutics. He is also developing new methods of producing pluripotent stem cells. He has a strong interest in anatomical biomechanics, where he is developing a novel hip joint, which is also patented. He supervises and has supervised several Ph.D’s and Masters students. He is regular post-graduate thesis examiner. He teaches undergraduate students in the Faculty of Medicine and Surgery and the Faculty of Health sciences in Gross Anatomy and Cell Biology. He also contributes to a number of post-graduate medical and scientific courses.</p>
-								</div>
+							<div class="col-6">
+								<img src="assets/img/cell.svg" width="100%" alt="">
 							</div>
 						</div>
 					</div>
+				</div>
+			</div>
+		</div>
+	</secion>
+
+	<section class="about-section">
+		<div class="container">
+			<div class="row d-flex align-items-stretch">
+				<div class="col-5">
+					<img src="assets/img/uom.png" alt="" class="invisible">
+				</div>
+				<div class="col-7">
+					<div class="content-section about-content">
+						<span style="font-family: gillsans"> <h2 style="color:#0B0062" class="mb-4">About</h2></span>
+						<span style="font-family: hoeflertext">
+							<p style="color:#0B0062">	- spin- out from the university <br>
+								- licensing to research market <br>
+								- development on protocol to infiltrate clinical market</p></span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="side-left" style="background-image: url(assets/img/uom.png)">
+
+		</div>
+	</section>
+
+	<section class="technology-section">
+		<div class="container">
+			<div class="row d-flex align-items-stretch">
+				<div class="row">
+					<div class="col-7">
+						<div class="content-section technology-content">
+							<span style="font-family: gillsans"> <h2 style="color:#FFFFFF" class="mb-4">Technology</h2></span>
+							<span style="font-family: hoeflertext">
+							<p style="color:#FFFFFF">	Our technology, involves a protocol for rapid cheap production of induced pluripotent stem cells without any genetic modification and within less than a month at a rate of about 90-100% conversion from cord blood mononuclear cells, as opposed to rates of 1% internationally. Apart from that, the agents required for conversion are cheap and very readily available, only requiring repurposing.
+							<br><br> This method is also being extended to include peripheral blood cells and will be further tested on fibroblasts. Developing a similarly successful rate of conversion will help to massively enhance the potential benefits due to being able to make these cells easily from any adult. </p>
+						</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="side-right" style="background-image: url(assets/img/techimage.png)">
+
+		</div>
+	</section>
+
+	<section class="novelty-section">
+		<div class="container">
+			<div class="row d-flex align-items-stretch">
+				<div class="col-5">
+				</div>
+				<div class="col-7">
+					<div class="content-section about-content">
+						<span style="font-family: gillsans"> <h2 style="color:#0B0062" class="mb-4">Novelty</h2></span>
+						<span style="font-family: hoeflertext">
+							<p style="color:#0B0062">	iPSCs could provide rapid availability of cells derived from each individual to produce any tissue types needed even possibly in advance, for prophylaxis. These cells would be the patient’s own cells, thus avoiding both rejection as well as dangerous immune invisibility.  The rapid development at low cost would make these cells viable both as widespread therapeutics as well as for scientific use, experimentation and research purposes.  One research possibility is producing stem cells for rare diseases and making tissues out of them.
+							<br><br> This brings the idea of personalised induced pluripotent stem cells into the realm of general public, for present or future regenerative medicine possibilities, which could target diseases such as Parkinsons disease, kidney disease and heart failure to name a few.
+						</p></span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="side-left" style="background-image: url(assets/img/result.png)">
+
+		</div>
+	</section>
+
+	<section class="inventors-section">
+		<div class="container pb-5 mb-5">
+			<div class="row">
+				<div class="col-12">
+					<div class="background-container">
+						<div class="container">
+							<div class="row">
+								<div class="col-10 offset-1">
+									<h2 class="mb-4 mt-3 pt-3 pb-4">Inventors</h2>
+								</div>
+								<div class="inventor row">
+									<div class="col-1 offset-1">
+
+									</div>
+									<div class="col-3">
+											<img src="assets/img/pierresw.svg" height="350px"/>
+									</div>
+
+									<div class="col-5 offset-1">
+										<span style="font-family: hoeflertext">
+										<h3 class="mb-4">Prof. Pierre Schembri Wismayer</h3>
+										<p class="mb-5">Prof Schembri-Wismayer has been involved in bringing in more than 1 million worth of grants funding to the University.. His research develops novel methods of immunotherapy and differentiation as cancer therapeutics. He is also developing new methods of producing pluripotent stem cells. He has a strong interest in anatomical biomechanics, where he is developing a novel hip joint, which is also patented. He supervises and has supervised several Ph.D’s and Masters students. He is regular post-graduate thesis examiner. He teaches undergraduate students in the Faculty of Medicine and Surgery and the Faculty of Health sciences in Gross Anatomy and Cell Biology. He also contributes to a number of post-graduate medical and scientific courses.</p>
+										</span>
+									</div>
+								</div>
+	  						<div class="mt-5 pt-3 inventor row">
+									<div class="col-1">
+									</div>
+						    	<div class="col-5">
+										<span style="font-family: hoeflertext">
+						    		<h3 class="mb-4"> Dr Ila Tewari Jasra</h3>
+										<p class="mb-5 pb=5">Dr Ila Tewari Jasra is an experienced researcher with a demonstrated history of working in the higher education industry. Skilled in Cell Culture, Flow Cytometry, Clinical Trials, Transfection, and culturing embryonic stem Cells. Strong research professional with a Master’s degree focused in Medical Microbiology, Bacteriology from Amity University Noida, PhD from University of Malta on “Derivation of induced pluripotent stem cells from cord blood".</p>
+									</span>
+									</div>
+									<div class="col-3 offset-1">
+											<img class="mb-5 pb=5" src="assets/img/pierresw.svg" height="350px"/>
+									</div>
+								</div>
+				<!-- <div class="background-container">
+	    </div> -->
+							</div>
+					</div>
+				</div>
 				</div>
 			</div>
 		</div>
@@ -68,7 +178,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12 col-md-3">
-					<img src="assets/img/logo.png" height="55px"/>
+					<img src="assets/img/DeNovo_Blue.svg" height="65px"/>
 				</div>
 				<div class="col-12 offset-0 col-md-5 offset-md-4">
 					<h3>Contact</h3>
