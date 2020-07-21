e7bc32211ff639cae46f411958b9c9da89bb0e5e
mifsudmarie <68011481+mifsudmarie@users.noreply.github.com>
2020-07-21 20:55:01 +0200

    Almost done

--- index.php
+++ index.php
@@ -1,15 +1,39 @@
+<!DOCTYPE html>
+<html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Primary Meta Tags -->
+	<title>De Novo Cell - Chemical production of induced pluripotent stem cells</title>
+	<meta name="title" content="De Novo Cell - Chemical production of induced pluripotent stem cells">
+	<meta name="description" content="Our technology, involves a protocol for rapid cheap production of induced pluripotent stem cells without any genetic modification and within less than a month at a rate of about 90-100% conversion from cord blood mononuclear cells, as opposed to rates of 1% internationally. ">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://denovocell.com/">
+	<meta property="og:title" content="De Novo Cell - Chemical production of induced pluripotent stem cells">
+	<meta property="og:description" content="Our technology, involves a protocol for rapid cheap production of induced pluripotent stem cells without any genetic modification and within less than a month at a rate of about 90-100% conversion from cord blood mononuclear cells, as opposed to rates of 1% internationally. ">
+	<meta property="og:image" content="">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="http://denovocell.com/">
+	<meta property="twitter:title" content="De Novo Cell - Chemical production of induced pluripotent stem cells">
+	<meta property="twitter:description" content="Our technology, involves a protocol for rapid cheap production of induced pluripotent stem cells without any genetic modification and within less than a month at a rate of about 90-100% conversion from cord blood mononuclear cells, as opposed to rates of 1% internationally. ">
+	<meta property="twitter:image" content="">
+
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
 </head>
 <body>
+	<a name="top"></a>
 		<section class="navbar-default bg-white">
 	<div class="container">
 	<nav class="navbar navbar-expand-lg navbar-light">
 	    <div class="d-flex flex-grow-1">
 	        <span class="w-100 d-lg-none d-block"><!-- hidden spacer to center brand on mobile --></span>
-	        <a class="navbar-brand d-none d-lg-inline-block" href="#">
-	            <img class=pr-4 mr-1 src="assets/img/DeNovo_Blue.svg" height="55px"/>
+	        <a class="navbar-brand d-none d-lg-inline-block">
+	            <img class="mt-2" src="assets/img/DeNovo_Blue.svg" height="55px"/ id="logo">
 	        </a>
 	        <a class="navbar-brand-two mx-auto d-lg-none d-inline-block" href="#">
 
@@ -20,9 +44,9 @@
 	            </button>
 	        </div>
 	    </div>
-	    <div class="pl-5 ml-5 collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+	    <div class="pl-5 ml-5 mt-3 collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
 				<span style="font-family: gillsans">
-	        <ul class="navbar-nav ml-auto flex-nowrap pl-5 ml-5">
+	        <ul class="navbar-nav ml-auto flex-nowrap pl-5 ml-5" style="color:#0B0062;font-size:18px" class="semi">
 	            <li class="nav-item">
 	                <a href="#about" class="nav-link m-2 menu-item nav-active">About</a>
 	            </li>
@@ -43,25 +67,25 @@
 	    </div>
 	</nav>
 		</section>
-	<section class="home">
+	<section class="home" id="home">
 		<div class="container pt-3 mt-4">
 			<div class="row">
 				<div class="col-12">
 					<div class="background-white">
 						<div class="row d-flex align-items-center">
-							<div class="col-5 offset-1">
-								<span style="font-family: gillsans"><h1 style="color:#0B0062;font-size:74px" class="semi";><i>de novo</i> Cell</h1>
+							<div class="col-lg-5 offset-1">
+								<span style="font-family: gillsans"><h1 style="color:#0B0062;font-size:85px" class="semi";><i>de novo</i> Cell</h1>
 								<div class="row d-flex align-items-center pb-5 mb-5">
 									<div class="col-4">
 										<hr class="divider">
 									</div>
 									<div class="col-8">
-										<h3 class="subtitle">Chemical production of induced pluripotent stem cells</h3></span>
+										<h3 style="color:#0B0062;font-size:20px" class="subtitle">Chemical production of induced pluripotent stem cells</h3></span>
 									</div>
 								</div>
 							</div>
 							<div class="col-6">
-								<img src="assets/img/cell.svg" width="100%" alt="">
+								<img  src="assets/img/cell.svg" width="100%" alt="">
 							</div>
 						</div>
 					</div>
@@ -78,17 +102,16 @@
 				</div>
 				<div class="col-7">
 					<div class="content-section about-content">
-						<span style="font-family: gillsans"> <h2 style="color:#0B0062" class="mb-4">About</h2></span>
+						<span style="font-family: gillsans"> <h2 style="color:#0B0062; font-size:34px" class="mb-4">About</h2></span>
 						<span style="font-family: hoeflertext">
-							<p style="color:#0B0062">	- spin- out from the university <br>
+							<p style="color:#0B0062;font-size:20px">	- spin- out from the university <br>
 								- licensing to research market <br>
 								- development on protocol to infiltrate clinical market</p></span>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="side-left" style="background-image: url(assets/img/uom.png)">
-
+		<div class="side-left" style="background-image: url(assets/img/uomweb@2x.png)">
 		</div>
 	</section>
 
@@ -98,9 +121,9 @@
 				<div class="row">
 					<div class="col-7">
 						<div class="content-section technology-content">
-							<span style="font-family: gillsans"> <h2 style="color:#FFFFFF" class="mb-4">Technology</h2></span>
+							<span style="font-family: gillsans"> <h2 style="color:#FFFFFF; font-size:34px" class="mb-4">Technology</h2></span>
 							<span style="font-family: hoeflertext">
-							<p style="color:#FFFFFF">	Our technology, involves a protocol for rapid cheap production of induced pluripotent stem cells without any genetic modification and within less than a month at a rate of about 90-100% conversion from cord blood mononuclear cells, as opposed to rates of 1% internationally. Apart from that, the agents required for conversion are cheap and very readily available, only requiring repurposing.
+							<p style="color:#FFFFFF;font-size:20px">	Our technology, involves a protocol for rapid cheap production of induced pluripotent stem cells without any genetic modification and within less than a month at a rate of about 90-100% conversion from cord blood mononuclear cells, as opposed to rates of 1% internationally. Apart from that, the agents required for conversion are cheap and very readily available, only requiring repurposing.
 							<br><br> This method is also being extended to include peripheral blood cells and will be further tested on fibroblasts. Developing a similarly successful rate of conversion will help to massively enhance the potential benefits due to being able to make these cells easily from any adult. </p>
 						</span>
 						</div>
@@ -108,7 +131,7 @@
 				</div>
 			</div>
 		</div>
-		<div class="side-right" style="background-image: url(assets/img/techimage.png)">
+		<div class="side-right blue-overlay" style="background-image: url(assets/img/techimage.png)">
 
 		</div>
 	</section>
@@ -120,17 +143,16 @@
 				</div>
 				<div class="col-7">
 					<div class="content-section about-content">
-						<span style="font-family: gillsans"> <h2 style="color:#0B0062" class="mb-4">Novelty</h2></span>
+						<span style="font-family: gillsans"> <h2 style="color:#0B0062; font-size:34px" class="mb-4">Novelty</h2></span>
 						<span style="font-family: hoeflertext">
-							<p style="color:#0B0062">	iPSCs could provide rapid availability of cells derived from each individual to produce any tissue types needed even possibly in advance, for prophylaxis. These cells would be the patient’s own cells, thus avoiding both rejection as well as dangerous immune invisibility.  The rapid development at low cost would make these cells viable both as widespread therapeutics as well as for scientific use, experimentation and research purposes.  One research possibility is producing stem cells for rare diseases and making tissues out of them.
+							<p style="color:#0B0062;font-size:20px">	iPSCs could provide rapid availability of cells derived from each individual to produce any tissue types needed even possibly in advance, for prophylaxis. These cells would be the patient’s own cells, thus avoiding both rejection as well as dangerous immune invisibility.  The rapid development at low cost would make these cells viable both as widespread therapeutics as well as for scientific use, experimentation and research purposes.
 							<br><br> This brings the idea of personalised induced pluripotent stem cells into the realm of general public, for present or future regenerative medicine possibilities, which could target diseases such as Parkinsons disease, kidney disease and heart failure to name a few.
 						</p></span>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="side-left" style="background-image: url(assets/img/result.png)">
-
+		<div class="side-left grey-overlay" style="background-image: url(assets/img/result.png)">
 		</div>
 	</section>
 
@@ -142,34 +164,41 @@
 						<div class="container">
 							<div class="row">
 								<div class="col-10 offset-1">
-									<h2 class="mb-4 mt-3 pt-3 pb-4">Inventors</h2>
+									<h2 class="mb-4 mt-3 pt-3 pb-2; font-size:34px">Inventors</h2>
 								</div>
-								<div class="inventor row">
+								<div class="inventor row pt-2">
 									<div class="col-1 offset-1">
-
+										<div class="inventor-position">
+											<hr>
+											Lead Inventor
+										</div>
 									</div>
 									<div class="col-3">
-											<img src="assets/img/pierresw.svg" height="350px"/>
+											<img class="mt-5 ml-3" src="assets/img/pierresw.svg" height="350px"/>
 									</div>
 
 									<div class="col-5 offset-1">
 										<span style="font-family: hoeflertext">
-										<h3 class="mb-4">Prof. Pierre Schembri Wismayer</h3>
-										<p class="mb-5">Prof Schembri-Wismayer has been involved in bringing in more than 1 million worth of grants funding to the University.. His research develops novel methods of immunotherapy and differentiation as cancer therapeutics. He is also developing new methods of producing pluripotent stem cells. He has a strong interest in anatomical biomechanics, where he is developing a novel hip joint, which is also patented. He supervises and has supervised several Ph.D’s and Masters students. He is regular post-graduate thesis examiner. He teaches undergraduate students in the Faculty of Medicine and Surgery and the Faculty of Health sciences in Gross Anatomy and Cell Biology. He also contributes to a number of post-graduate medical and scientific courses.</p>
+										<b><h3 class="mb-4">Prof. Pierre Schembri Wismayer</h3></b>
+										<p class="mb-5"; style="font-size:18px">Prof Schembri-Wismayer has been involved in bringing in more than 1 million worth of grants funding to the University.. His research develops novel methods of immunotherapy and differentiation as cancer therapeutics. He is also developing new methods of producing pluripotent stem cells. He has a strong interest in anatomical biomechanics, where he is developing a novel hip joint, which is also patented. He supervises and has supervised several Ph.D’s and Masters students. He is regular post-graduate thesis examiner. He teaches undergraduate students in the Faculty of Medicine and Surgery and the Faculty of Health sciences in Gross Anatomy and Cell Biology. He also contributes to a number of post-graduate medical and scientific courses.</p>
 										</span>
 									</div>
 								</div>
 	  						<div class="mt-5 pt-3 inventor row">
-									<div class="col-1">
-									</div>
-						    	<div class="col-5">
+						    	<div class="col-5 offset-1">
 										<span style="font-family: hoeflertext">
-						    		<h3 class="mb-4"> Dr Ila Tewari Jasra</h3>
-										<p class="mb-5 pb=5">Dr Ila Tewari Jasra is an experienced researcher with a demonstrated history of working in the higher education industry. Skilled in Cell Culture, Flow Cytometry, Clinical Trials, Transfection, and culturing embryonic stem Cells. Strong research professional with a Master’s degree focused in Medical Microbiology, Bacteriology from Amity University Noida, PhD from University of Malta on “Derivation of induced pluripotent stem cells from cord blood".</p>
+						    		<b><h3 class="mb-4 pl-3"> Dr Ila Tewari Jasra</h3></b>
+										<p class="mb-5 pl-3"; style="font-size:18px">Dr Ila Tewari Jasra is an experienced researcher with a demonstrated history of working in the higher education industry. Skilled in Cell Culture, Flow Cytometry, Clinical Trials, Transfection, and culturing embryonic stem Cells. Strong research professional with a Master’s degree focused in Medical Microbiology, Bacteriology from Amity University Noida, PhD from University of Malta on “Derivation of induced pluripotent stem cells from cord blood".</p>
 									</span>
 									</div>
 									<div class="col-3 offset-1">
 											<img class="mb-5 pb=5" src="assets/img/pierresw.svg" height="350px"/>
+									</div>
+									<div class="col-1">
+										<div class="inventor-position">
+											<hr>
+											Co-Inventor
+										</div>
 									</div>
 								</div>
 				<!-- <div class="background-container">
@@ -185,10 +214,10 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-12 col-md-3">
-					<img src="assets/img/DeNovo_Blue.svg" height="65px"/>
+						<a href="#top"><img src="assets/img/DeNovo_Blue.svg" height="55px"></a>
 				</div>
 				<div class="col-12 offset-0 col-md-5 offset-md-4">
-					<h3>Contact</h3>
+					<h3 class="font-size:34px">Contact</h3>
 					<p>
 						Licensing and Industrial Partnerships:<br>
 						Marie Mifsud<br>
@@ -201,3 +230,4 @@
 		</div>
 	</footer>
 </body>
+</html>
